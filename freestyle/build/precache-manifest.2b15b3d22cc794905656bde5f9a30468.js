7a4a5d904868dbeeac21533f8bd59d19cf925376
Salomonrey <36483352+Salomonrey@users.noreply.github.com>
2019-07-19 14:45:05 +0600

    Salih

---
+++ freestyle/build/precache-manifest.2b15b3d22cc794905656bde5f9a30468.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f12b54da037c60e1a0088b7b2acd4a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "047c8dea1e71141b5ac8",
+    "url": "/static/css/main.bc788dce.chunk.css"
+  },
+  {
+    "revision": "ba0b3ea30cc62be07df7",
+    "url": "/static/js/2.3feb8857.chunk.js"
+  },
+  {
+    "revision": "047c8dea1e71141b5ac8",
+    "url": "/static/js/main.d6973f6c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12c5024f5e7b1ab1d86a32d051f560f5",
+    "url": "/static/media/BebasNeueBold.12c5024f.ttf"
+  },
+  {
+    "revision": "85b13a3b9a2208e1027c0cab883b501f",
+    "url": "/static/media/BebasNeueRegular.85b13a3b.ttf"
+  },
+  {
+    "revision": "7c692ffc3d63e41a6802134772a4f872",
+    "url": "/static/media/Blogimg.7c692ffc.png"
+  },
+  {
+    "revision": "9bee19aa51c880cb0d3c0f9659b15d68",
+    "url": "/static/media/Blogimgipad.9bee19aa.png"
+  },
+  {
+    "revision": "6b28c5938d1e4c3f29ef00f27b021303",
+    "url": "/static/media/Circe-Regular.6b28c593.ttf"
+  },
+  {
+    "revision": "84acf3b1aa009025f7781d021dce9268",
+    "url": "/static/media/Desskttop.84acf3b1.png"
+  },
+  {
+    "revision": "3d373a63026d49451ecd08b27c3596d1",
+    "url": "/static/media/Divanuui.3d373a63.png"
+  },
+  {
+    "revision": "a186c3913de6e608d228400972dc65f0",
+    "url": "/static/media/FB.a186c391.png"
+  },
+  {
+    "revision": "54c00682b93300b2de35fb4bf317bfa7",
+    "url": "/static/media/Frees.54c00682.mp4"
+  },
+  {
+    "revision": "6b0255f18cd9907c72d660d01e72c2e5",
+    "url": "/static/media/Gray2.6b0255f1.png"
+  },
+  {
+    "revision": "b7ff87304d44c6b6f3ba0d39e660de5c",
+    "url": "/static/media/Group123.b7ff8730.png"
+  },
+  {
+    "revision": "e5be53f1b8248f98c88e8a25d37a425f",
+    "url": "/static/media/Hole.e5be53f1.png"
+  },
+  {
+    "revision": "1bec9c265907f05322643e72f27fe036",
+    "url": "/static/media/Homeles.1bec9c26.png"
+  },
+  {
+    "revision": "68d57c92d74ffb0191e2d86ff426a0a4",
+    "url": "/static/media/IN.68d57c92.png"
+  },
+  {
+    "revision": "afa0ebfbcb5b3ac0e500f561da56764b",
+    "url": "/static/media/Ipadamain.afa0ebfb.png"
+  },
+  {
+    "revision": "33aa8ebcc58fc4757dca30e2df26f523",
+    "url": "/static/media/Ipadhpne.33aa8ebc.png"
+  },
+  {
+    "revision": "8511479fbb67d653fcd45a132b2926b6",
+    "url": "/static/media/Krovatit.8511479f.png"
+  },
+  {
+    "revision": "d8f819b2d8786860ea52f6c76c1fa19d",
+    "url": "/static/media/Kuhna.d8f819b2.png"
+  },
+  {
+    "revision": "3198308615905371ad9918fb36027261",
+    "url": "/static/media/Matrasci.31983086.png"
+  },
+  {
+    "revision": "98402200db8eb592e1442d67436e4ff6",
+    "url": "/static/media/PT.98402200.png"
+  },
+  {
+    "revision": "da7243b1295156355cb0db877be2dbd7",
+    "url": "/static/media/Phonename.da7243b1.png"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "df8c626474a73ab7a8b511655597c7c4",
+    "url": "/static/media/ProximaNova-Semibold.df8c6264.ttf"
+  },
+  {
+    "revision": "64f8b35b6c80f4b77fdc090d3cd9f616",
+    "url": "/static/media/TTNorms-Bold.64f8b35b.ttf"
+  },
+  {
+    "revision": "1082eb3bf7be09cb7eea9fd3c057732e",
+    "url": "/static/media/TTNorms-Medium.1082eb3b.ttf"
+  },
+  {
+    "revision": "eb6edc3979c854b8a687a8b973dda56a",
+    "url": "/static/media/TTNorms-Regular.eb6edc39.ttf"
+  },
+  {
+    "revision": "55ebb559e80f3219945fb97044d02d63",
+    "url": "/static/media/Vannaua.55ebb559.png"
+  },
+  {
+    "revision": "9015871a358790e4efdbf4736bf5fc30",
+    "url": "/static/media/phoneimg.9015871a.png"
+  }
+]);
